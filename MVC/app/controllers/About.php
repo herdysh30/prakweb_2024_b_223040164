aa29567194a72675ea8f817af9403017cae97cce
herdysh30 <herdy.syaban@gmail.com>
2024-10-17 15:46:46 +0700

    #4 Controller

---
+++ MVC/app/controllers/About.php
@@ -0,0 +1,9 @@
+<?php
+class About{
+    public function index($nama = 'Herdy', $pekerjaan = 'Mahasiswa'){
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
